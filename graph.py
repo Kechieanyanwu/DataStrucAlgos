bb8ee0612573e2f87226f427527e3a365f5fa732
Nkechi Anyanwu <kechie,94@gmail.com>
2023-11-17 11:10:52 +0000

    Include graph

---
+++ graph.py
@@ -0,0 +1,10 @@
+graph = {}
+
+graph["you"] = ["claire", "bob", "alice"]
+graph["claire"] = ["Thom", "johnny"]
+graph["alice"] = ["peggy"]
+graph["bob"] = ["anuj", "peggy"]
+graph["anuj"] = []  
+graph["peggy"] = []  
+graph["johnny"] = []  
+graph["thom"] = []  
